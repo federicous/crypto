3a43bfb603801b7edb13c869b6e4b198317f58dd
Federico Yemes <federicous@gmail.com>
2021-08-06 18:05:37 -0300

    agregado de codigo boostral y ajustes del script inversor

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-/* El siguiente programa simula una inversión en criptomonedas
-dependiendo de la variación del precio de la criptomoneda se realiza
-la finalización de la operación ya sea porque sobrepasó el limite de ganancia
-o porque descendió por debajo del limite de pérdida
+/* El siguiente programa simula una inversión en criptomonedas comenzando con una cantidad
+de dinero en dolares a invertir y un valor inicial de la criptomoneda
+Una vez ingresados los valores anteriores se va a solicitar la actualización del valor
+de la criptomoneda en dolares, dependiendo de la variación del precio de la criptomoneda se realiza
+la finalización de la operación ya sea porque sobrepasó el limite de ganancia (take profit)
+o porque descendió por debajo del limite de pérdida (stop loss), estos limites estan previamente
+fijados como valores constantes
 */
 
 // let billetera = parseInt(prompt("Ingrese la cantidad de criptomonedas disponible en su billetera"));
@@ -13,14 +16,14 @@ let takeProfit = 30;
 // Porcentaje de baja de precio para realizar una venta y dejar de perder dinero
 let stopLoss = 5;
 
-alert("Los limites para finalizar la operación son los siguientes: \n Take Profit= " + takeProfit + "% \n Stop Loss= " + stopLoss + "%")
+alert("Los limites que finalizan la operación son los siguientes: \n Take Profit= " + takeProfit + "% \n Stop Loss= " + stopLoss + "%")
 
-function compra(dinero, precio) {
+function comprar(dinero, precio) {
 	let resultado = dinero / precio;
 	return resultado;
 }
 
-function vender(crypto, precio, billetera) {
+function vender(crypto, precio) {
 	let resultado = crypto * precio;
 	return resultado;
 }
@@ -29,7 +32,7 @@ function porcentajeCambio(precioAnterior, precioNuevo) {
 	return Math.abs(((precioNuevo * 100) / precioAnterior) - 100);
 }
 
-let criptoComprada = compra(dineroInvertido, precioInicial);
+let criptoComprada = comprar(dineroInvertido, precioInicial);
 // billetera = billetera + criptoComprada;
 let dineroTotal = 0;
 // Loop para actualizar el precio de la criptomoneda
@@ -47,7 +50,9 @@ while (true) {
 	}
 }
 if (dineroTotal < dineroInvertido) {
-	alert("La operación finalizó por alcanzar el Stop Loss \n Dinero disponible en dolares: " + dineroTotal)
-} else {
-	alert("La operación finalizó por alcanzar el Take Profit \n Dinero disponible en dolares: " + dineroTotal)
+	alert("La operación finalizó por alcanzar el Stop Loss \n Dinero disponible en dolares: " + dineroTotal);
+} else if (dineroTotal>dineroInvertido){
+	alert("La operación finalizó por alcanzar el Take Profit \n Dinero disponible en dolares: " + dineroTotal);
+}else {
+	alert("La operación fue cancelada  \n Dinero disponible en dolares: " + dineroTotal);
 }
