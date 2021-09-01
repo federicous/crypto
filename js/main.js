1243f595637b143ee82b444f4075615bbfbea4e6
fede <federicous@gmail.com>
2021-09-01 03:16:34 -0300

    se validan los datos, corrección de avisos

--- js/main.js
+++ js/main.js
@@ -110,16 +110,19 @@ bontonInvertir.addEventListener("click", invertir);
 
 function invertir() {
 	quitarAviso();
-	datosOperacion();
-	precioActualizado = precioInicial;
-	aviso("Operación en curso")
-	visualizarDatos("Compra:", `${criptomoneda.cantidad} ${cryptoName}`);
-	operar();
-	guardar();
-	bontonInvertir.disabled = true;
-	desbloquearBoton("botonActualizaInput");
-	desbloquearBoton("botonCancelar");
-	desbloquearBoton("precioActual");
+	ocultarDatos();
+	if (validacion()) {
+		datosOperacion();
+		precioActualizado = precioInicial;
+		aviso("Operación en curso")
+		visualizarDatos("Compra:", `${criptomoneda.cantidad} ${cryptoName}`);
+		operar();
+		guardar();
+		bontonInvertir.disabled = true;
+		desbloquearBoton("botonActualizaInput");
+		desbloquearBoton("botonCancelar");
+		desbloquearBoton("precioActual");
+	}
 }
 /* ################### Fin Boton Invertir ###################### */
 
@@ -128,11 +131,13 @@ let botonActualizaInput = document.getElementById("botonActualizaInput");
 botonActualizaInput.addEventListener("click", actualizar);
 
 function actualizar() {
-	if (operacion.finalizada == false) {
-		let precioActual = document.getElementById("precioActual");
-		precioActualizado = precioActual.value;
-		operar();
-		guardar();
+	if (validaNuevoPrecio()) {
+		if (operacion.finalizada == false) {
+			let precioActual = document.getElementById("precioActual");
+			precioActualizado = precioActual.value;
+			operar();
+			guardar();
+		}
 	}
 }
 /* ################### Fin Boton Actualizar ###################### */
@@ -144,9 +149,8 @@ botonCancelar.addEventListener("click", cancelar);
 function cancelar() {
 	if (operacion.finalizada == false) {
 		operacion.cancelar();
-		aviso("Operacion Cancelada!!!","alert");
-		guardar();
 		reiniciarForm();
+		guardar();
 	}
 }
 /* ################### Fin Boton Cancelar ###################### */
@@ -165,16 +169,7 @@ function datosOperacion() {
 	precioInicial = document.getElementById("precioInput").value;
 	/* ################### Fin Lectura de Datos ###################### */
 
-	let valoresNumericos = [dineroInvertido, takeProfit, stopLoss, precioInicial];
 
-	for (const x of valoresNumericos) {
-		let text;
-		if (isNaN(x) || x < 1 || x > 10) {
-			text = "Input not valid";
-		} else {
-			text = "Input OK";
-		}
-	}
 	criptomoneda = new Crypto(cryptoName, precioInicial);
 	criptomoneda.comprar(dineroInvertido);
 
@@ -182,18 +177,45 @@ function datosOperacion() {
 	operacion = new Inversion(dineroInvertido, takeProfit, stopLoss, fechaHora, criptomoneda);
 }
 
-function operar() {
-	if (precioActualizado == "ESC") {
-		operacion.cancelar();
-		alert("operacion cancelada");
-	} else {
-		let precioNuevo = parseInt(precioActualizado);
-		criptomoneda.actualizar(precioNuevo);
-		let cambioPrecio = criptomoneda.porcentajeCambio();
-		if (((precioNuevo > precioInicial) && (cambioPrecio >= takeProfit)) || ((precioNuevo < precioInicial) && (cambioPrecio >= stopLoss))) {
-			operacion.finalizar();
-			reiniciarForm();
+function validacion() {
+
+	cryptoName = document.getElementById("cryptoSelect");
+	dineroInvertido = document.getElementById("dineroInput");
+	takeProfit = document.getElementById("takeProfitInput");
+	stopLoss = document.getElementById("stopLossInput");
+	precioInicial = document.getElementById("precioInput");
+	let valoresNumericos = [precioInicial, dineroInvertido, takeProfit, stopLoss];
+	quitarAviso();
+	let contador = 0;
+	for (const x of valoresNumericos) {
+		if (isNaN(x.value) || x.value < 1) {
+			aviso(`${x.name} No válido!!`, "alert");
+			contador += 1;
 		}
+	}
+	if (contador != 0) {
+		return false;
+	}
+	return true;
+}
+
+function validaNuevoPrecio() {
+	quitarAviso();
+	let precioActual = document.getElementById("precioActual");
+	if (isNaN(precioActual.value) || precioActual.value < 1) {
+		aviso(`${precioActual.name} No válido!!`, "alert");
+		return false;
+	}
+	return true;
+}
+
+function operar() {
+	let precioNuevo = parseInt(precioActualizado);
+	criptomoneda.actualizar(precioNuevo);
+	let cambioPrecio = criptomoneda.porcentajeCambio();
+	if (((precioNuevo > precioInicial) && (cambioPrecio >= takeProfit)) || ((precioNuevo < precioInicial) && (cambioPrecio >= stopLoss))) {
+		operacion.finalizar();
+		reiniciarForm();
 	}
 }
 
@@ -206,10 +228,13 @@ function guardar() {
 
 		// Reporto el resultado
 		if (operacion.dineroTotal < operacion.dineroInvertido && operacion.estado != "Cancelado") {
+			aviso("Operacion Finalizada!!!");
 			visualizarDatos("Alcanzó el Stop Loss", `Dinero total: $${operacion.dineroTotal}`);
 		} else if (operacion.dineroTotal > operacion.dineroInvertido && operacion.estado != "Cancelado") {
+			aviso("Operacion Finalizada!!!");
 			visualizarDatos("Alcanzó el Take Profit", `Dinero total: $${operacion.dineroTotal}`);
 		} else {
+			aviso("Operacion Cancelada!!!", "alert");
 			visualizarDatos("La operación fue cancelada", `Dinero total: $${operacion.dineroTotal}`);
 		}
 
@@ -267,18 +292,19 @@ function reiniciarForm() {
 function aviso(mensaje, tipo) {
 	if (tipo == "alert") {
 		$("#formulario").append(`
-		<div id="aviso" class="alert alert-danger d-flex align-items-center mt-3" role="alert">
+		<div class="avisos alert alert-danger alert-dismissible fade show d-flex align-items-center mt-3" role="alert">
  	 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
  	  	 		<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
  	 		</svg>
 		  	<div>
 		   	 ${mensaje}
 		  	</div>
+			<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 		</div>
 		`)
 	} else {
 		$("#formulario").append(`
-		<div id="aviso" class="alert alert-primary d-flex align-items-center mt-3" role="alert">
+		<div class="avisos alert alert-primary d-flex align-items-center mt-3" role="alert">
  	 		<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
  	  	 		<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
  	 		</svg>
@@ -291,16 +317,20 @@ function aviso(mensaje, tipo) {
 }
 
 function quitarAviso() {
-	$("#aviso").remove();
+	$(".avisos").remove();
 }
 
 function visualizarDatos(titulo, datos) {
-	$("#datos").remove();
-	$("#formulario").append(`<div id="datos" class="card text-dark bg-light my-3 w-100">
+	$(".datos").remove();
+	$("#formulario").append(`<div class="datos card text-dark bg-light my-3 w-100">
 	<div class="card-header">${titulo}</div>
 	<div class="card-body">
 	  <p class="card-text">${datos}
 		</p>
 	</div>
       </div>`)
+}
+
+function ocultarDatos() {
+	$(".datos").remove();
 }
