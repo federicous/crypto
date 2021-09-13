fabb27f5aadf36b8fee6fa2277e0456b57bfdead
fede <federicous@gmail.com>
2021-09-13 00:37:49 -0300

    habilito grafico btc

--- js/animaciones.js
+++ js/animaciones.js
@@ -1,13 +1,12 @@
-$(function(){
-$(".logoPrincipal").fadeIn(2000,function () {
-	$(".ojos").fadeIn(1500);
-});
-$(".logoTexto").fadeIn(2000);
-$(".botonInicio").fadeIn().click(function () {
-	$(".botonInicio").hide(1000);
-	$(".credenciales").slideDown();
-	$(".accesoPrueba").fadeIn();
-});
-//   $("#cuerpo").fadeIn("slow");
+$(function () {
+	$(".logoPrincipal").fadeIn(1500, function () {
+		$(".ojos").fadeIn(1000);
+	});
+	$(".logoTexto").fadeIn(1500);
+	$(".botonInicio").fadeIn().click(function () {
+		$(".botonInicio").hide(1000);
+		$(".credenciales").slideDown();
+		$(".accesoPrueba").fadeIn();
+	});
 
 });
