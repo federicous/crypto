6574d375c4505fbd9f3b3d5bd5d18a00be7e2e37
fede <federicous@gmail.com>
2021-09-23 00:47:52 -0300

    wallet2 inicio

---
+++ js/wallet.js
@@ -0,0 +1,42 @@
+/* #################### Genero una tabla con los datos de las inversiones ####################### */
+
+// let historialInversiones = JSON.parse(localStorage.getItem("cuenta"));
+// let tabla = document.getElementById("tableBody");
+
+// for (let op = 0; op < historialInversiones.length; op++) {
+// 	let nuevaFila = document.createElement("tr");
+// 	nuevaFila.innerHTML = `<td>${op+1}</td>
+// 			<td>${historialInversiones[op].fechaHora}</td>
+// 			<td>${historialInversiones[op].nombre}</td>
+// 			<td>$${historialInversiones[op].dineroInvertido}</td>
+// 			<td>${historialInversiones[op].saldoPorcentaje}%</td>
+// 			<td>${historialInversiones[op].estado}</td>`;
+// 	tabla.appendChild(nuevaFila);
+// }
+// $('.table').tablesorter();
+
+let tabla = document.getElementById("tableBody");
+$.get("./data/cuentas.json", function (respuesta, estado) {
+	if (estado === "success") {
+		const listaCuentas = JSON.parse(respuesta);
+		console.log(listaCuentas);
+		for (const user of respuesta) {
+			if (user.usuario == "admin") {
+				console.log(user);
+				for (const moneda in user.wallet) {
+
+
+					let nuevaFila = document.createElement("tr");
+					nuevaFila.innerHTML = `
+					<td>${moneda}</td>
+					<td>${Object.values(moneda)}</td>
+					<td>${user}</td>`;
+					tabla.appendChild(nuevaFila);
+				}
+			}
+
+		}
+	}
+	// console.log(respuesta);
+
+});
