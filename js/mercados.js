7a196b2cf46e38246c374a7ea1bea8f939bc43ad
fede <federicous@gmail.com>
2021-09-24 00:06:03 -0300

    script mercados

--- js/mercados.js
+++ js/mercados.js
@@ -1,52 +1,66 @@
-/* #################### Genero una grafica por cada criptomonedas que posee el usuario actual ####################### */
+let cryptoSeleccion;
 
-let usuarioActivo = JSON.parse(localStorage.getItem("usuario"));
-let precioMoneda;
-let tabla = document.getElementById("tableBody");
+/* ################## Genero la lista de criptomonedas para seleccionar ##################### */
 
-$(document).ready(function () {
+$(function () {
 
-	$.get("./data/cuentas.json", function (respuesta, estado) {
+	let currencies;
+	let currenciesList = [];
+	const URLGETCUR = "https://api.pro.coinbase.com/currencies"
+	$.get(URLGETCUR, function (respuesta, estado) {
 		if (estado === "success") {
-			for (const user of respuesta) {
-				if (user.usuario == usuarioActivo) {
-					console.log(user);
-					for (const moneda in user.wallet) {
-
-
-						$("#tradingView").append(`
-							<!-- TradingView Widget BEGIN -->
-							<div class="tradingview-widget-container">
-							  <div id="tradingview_aff49" class="tradingViewGrafico"></div>
-							  <div class="tradingview-widget-copyright"><a href="https://es.tradingview.com/symbols/${moneda}USD/?exchange=COINBASE" rel="noopener" target="_blank"><span class="blue-text">Gráfico</span></a> por TradingView</div>
-							  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-							  <script type="text/javascript">
-							  new TradingView.widget(
-							  {
-							  "autosize": true,
-							  "symbol": "COINBASE:${moneda}USD",
-							  "interval": "60",
-							  "timezone": "Etc/UTC",
-							  "theme": "light",
-							  "style": "1",
-							  "locale": "es",
-							  "toolbar_bg": "#f1f3f6",
-							  "enable_publishing": false,
-							  "allow_symbol_change": true,
-							  "container_id": "tradingview_aff49"
-							}
-							  );
-							  </script>
-							</div>
-							<!-- TradingView Widget END -->
-								`);
-
-
-					}
+			currencies = respuesta;
+			for (const moneda of currencies) {
+				if (moneda.details.type == "crypto") {
+					currenciesList.push(moneda.id);
 				}
-
 			}
-		}
+			currenciesList.sort();
+			let seleccion = document.getElementById("cryptoSelect");
 
+			for (let coin of currenciesList) {
+				let opcion = document.createElement("option");
+				opcion.value = coin;
+				opcion.text = coin;
+				seleccion.appendChild(opcion);
+			}
+			$("option[value='BTC']").attr("selected", "selected");
+			$("#cryptoSelect").trigger("change");
+		};
 	});
+});
+
+
+
+
+
+$("#cryptoSelect").change(() => {
+	cryptoSeleccion = $("#cryptoSelect").val();
+	$("#tradingViewSection").append(`
+	<!-- TradingView Widget BEGIN -->
+	<div class="tradingview-widget-container">
+	  <div id="tradingview_aff49" class="tradingViewGrafico"></div>
+	  <div class="tradingview-widget-copyright"><a href="https://es.tradingview.com/symbols/${cryptoSeleccion}USD/?exchange=COINBASE" rel="noopener" target="_blank"><span class="blue-text">Gráfico</span></a> por TradingView</div>
+	  <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+	  <script type="text/javascript">
+	  new TradingView.widget(
+	  {
+	  "autosize": true,
+	  "symbol": "COINBASE:${cryptoSeleccion}USD",
+	  "interval": "60",
+	  "timezone": "Etc/UTC",
+	  "theme": "light",
+	  "style": "1",
+	  "locale": "es",
+	  "toolbar_bg": "#f1f3f6",
+	  "enable_publishing": false,
+	  "allow_symbol_change": true,
+	  "container_id": "tradingview_aff49"
+	}
+	  );
+	  </script>
+	</div>
+	<!-- TradingView Widget END -->
+		`)
+
 });
