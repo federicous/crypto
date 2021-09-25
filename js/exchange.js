2210671a739f0b20d79945b56c9827546a1a85be
Federico Yemes <federicous@gmail.com>
2021-09-24 21:34:04 -0300

    precio doble

--- js/exchange.js
+++ js/exchange.js
@@ -1,5 +1,6 @@
 $(function () {
 
+	/* ################ Lista de criptomonedas disponibles en la wallet ################# */
 	let currenciesList = [];
 	let usuarioActivo = JSON.parse(localStorage.getItem("usuario"));
 	let precioMoneda;
@@ -27,6 +28,7 @@ $(function () {
 
 	});
 
+	/* ###################### Lista de criptomonedas posibles de intercambio ##################### */
 
 	let currencies;
 	let currenciesList2 = [];
@@ -53,28 +55,77 @@ $(function () {
 		};
 	});
 
+	let cryptoNameOrigen;
+	let cryptoNameDestino;
+	let URLGETORIGEN;
+	let URLGETDESTINO;
+	/* ################### Eventos de seleccion de criptomonedas a intercambiar ################# */
+	$("#cryptoSelect").change(() => {
+		cryptoNameOrigen = $("#cryptoSelect").val();
+		$(".unidadOrigen").text(cryptoNameOrigen);
+		URLGETORIGEN = `https://api.coinbase.com/v2/prices/${cryptoNameOrigen}-USD/buy`
 
-	/* 	let currencies;
-		const URLGETCUR = "https://api.pro.coinbase.com/currencies"
-		$.get(URLGETCUR, function (respuesta, estado) {
+		$.get("./data/cuentas.json", function (respuesta, estado) {
 			if (estado === "success") {
-				currencies = respuesta;
-				for (const moneda of currencies) {
-					if (moneda.details.type == "crypto") {
-						currenciesList.push(moneda.id);
+				for (const user of respuesta) {
+					if (user.usuario == usuarioActivo) {
+						console.log(user.usuario);console.log("pepe");
+						for (const moneda in user.wallet) {
+							if (moneda == cryptoNameOrigen) {console.log("pepe2");
+								console.log(user.wallet[moneda]);
+								localStorage.setItem("cryptoOrigenCantidad", JSON.stringify(user.wallet[moneda]));
+								console.log(respuesta.data.amount);
+								coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigenCantidad"));
+								console.log(coinbaseOrigen);
+							}
+	
+						}
 					}
+	
 				}
-				currenciesList.sort();
-				let seleccion = document.getElementById("cryptoSelect");
+			}
+	
+		});
+	});
 
-				for (let coin of currenciesList) {
-					let opcion = document.createElement("option");
-					opcion.value = coin;
-					opcion.text = coin;
-					seleccion.appendChild(opcion);
-				}
-				$("#cryptoSelect option[value='BTC']").attr("selected", "selected");
-				$("#cryptoSelect").trigger("change");
-			};
-		}); */
+	$("#cryptoSelect2").change(() => {
+		cryptoNameDestino = $("#cryptoSelect2").val();
+		$(".unidadDestino").text(cryptoNameDestino);
+		URLGETDESTINO = `https://api.coinbase.com/v2/prices/${cryptoNameDestino}-USD/buy`
+	});
+
+	/* ##################### Actualizacion de precios en dolares ######################### */
+	let coinbaseOrigen;
+	let coinbaseDestino;
+	setInterval(() => {
+		$.get(URLGETORIGEN, function (respuesta, estado) {
+			if (estado === "success") {
+	coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigenCantidad"));
+				$("#origenInput").attr("value", `${coinbaseOrigen} (${coinbaseOrigen*respuesta.data.amount} USD)`);
+				$("#origenInput").trigger("change");
+			}
+		});
+		$.get(URLGETDESTINO, function (respuesta, estado) {
+			if (estado === "success") {
+				$("#destinoInput").attr("value", respuesta.data.amount);
+				$("#destinoInput").trigger("change");
+				// console.log(respuesta.data.amount);
+				localStorage.setItem("cryptoOrigen", JSON.stringify(respuesta.data.amount));
+			}
+		});
+
+	}, 5000);
+	// coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigen"));
+	// console.log(coinbaseOrigen);
+
+	// $.get("./data/cuentas.json", function (respuesta, estado) {
+	// 	if (estado === "success") {
+	// 		console.log(respuesta[1].usuario);
+	// 	}
+	// });
+
+	console.log("pepe");
+
+
+
 });
