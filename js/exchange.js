0d9c91579379523ea7204236265a8f5f2ae55085
Federico Yemes <federicous@gmail.com>
2021-09-28 15:17:31 -0300

    conversion entre criptos

--- js/exchange.js
+++ js/exchange.js
@@ -69,29 +69,53 @@ $(function () {
 			if (estado === "success") {
 				for (const user of respuesta) {
 					if (user.usuario == usuarioActivo) {
-						console.log(user.usuario);console.log("pepe");
 						for (const moneda in user.wallet) {
-							if (moneda == cryptoNameOrigen) {console.log("pepe2");
-								console.log(user.wallet[moneda]);
+							if (moneda == cryptoNameOrigen) {
 								localStorage.setItem("cryptoOrigenCantidad", JSON.stringify(user.wallet[moneda]));
-								console.log(respuesta.data.amount);
 								coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigenCantidad"));
-								console.log(coinbaseOrigen);
 							}
-	
+
 						}
 					}
-	
+
 				}
 			}
-	
+
 		});
 	});
 
+	let monedaPresente = false;
 	$("#cryptoSelect2").change(() => {
 		cryptoNameDestino = $("#cryptoSelect2").val();
 		$(".unidadDestino").text(cryptoNameDestino);
 		URLGETDESTINO = `https://api.coinbase.com/v2/prices/${cryptoNameDestino}-USD/buy`
+
+		$.get("./data/cuentas.json", function (respuesta, estado) {
+			if (estado === "success") {
+				for (const user of respuesta) {
+					if (user.usuario == usuarioActivo) {
+						for (const moneda in user.wallet) {
+							// console.log(moneda);
+							if (moneda == cryptoNameDestino) {
+								localStorage.setItem("cryptoDestinoCantidad", JSON.stringify(user.wallet[moneda]));
+								coinbaseDestino = JSON.parse(localStorage.getItem("cryptoDestinoCantidad"));
+								monedaPresente = true;
+							}
+
+						}
+						/* Si la moneda no esta presente en la cartera aparece con cantidad "0" */
+						if (monedaPresente == false) {
+							localStorage.setItem("cryptoDestinoCantidad", JSON.stringify("0"));
+							console.log(cryptoNameDestino);
+							coinbaseDestino = JSON.parse(localStorage.getItem("cryptoDestinoCantidad"));
+
+						}
+					}
+
+				}
+			}
+
+		});
 	});
 
 	/* ##################### Actualizacion de precios en dolares ######################### */
@@ -100,32 +124,87 @@ $(function () {
 	setInterval(() => {
 		$.get(URLGETORIGEN, function (respuesta, estado) {
 			if (estado === "success") {
-	coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigenCantidad"));
-				$("#origenInput").attr("value", `${coinbaseOrigen} (${coinbaseOrigen*respuesta.data.amount} USD)`);
+				coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigenCantidad"));
+				$("#origenInput").attr("value", `${coinbaseOrigen}`);
+				$("#origenInputUSD").attr("value", `${coinbaseOrigen*respuesta.data.amount}`);
 				$("#origenInput").trigger("change");
+				localStorage.setItem("cryptoOrigenUSD", JSON.stringify(`${coinbaseOrigen*respuesta.data.amount}`));
 			}
 		});
 		$.get(URLGETDESTINO, function (respuesta, estado) {
 			if (estado === "success") {
-				$("#destinoInput").attr("value", respuesta.data.amount);
+				coinbaseDestino = JSON.parse(localStorage.getItem("cryptoDestinoCantidad"));
+				$("#destinoInput").attr("value", `${coinbaseDestino}`);
+				$("#destinoInputUSD").attr("value", `${coinbaseDestino*respuesta.data.amount}`);
 				$("#destinoInput").trigger("change");
 				// console.log(respuesta.data.amount);
-				localStorage.setItem("cryptoOrigen", JSON.stringify(respuesta.data.amount));
+				localStorage.setItem("cryptoDestinoUSD", JSON.stringify(`${coinbaseDestino*respuesta.data.amount}`));
 			}
 		});
 
 	}, 5000);
-	// coinbaseOrigen = JSON.parse(localStorage.getItem("cryptoOrigen"));
-	// console.log(coinbaseOrigen);
 
-	// $.get("./data/cuentas.json", function (respuesta, estado) {
-	// 	if (estado === "success") {
-	// 		console.log(respuesta[1].usuario);
-	// 	}
-	// });
-
-	console.log("pepe");
+let resultadoOrigen;
+let resultadoDestino;
+let cantidadOrigen;
+let cantidadDestino;		
+let cantidadConvertir;
+let cantidadConvertirName;
 
 
+	$("#botonConvertir").click(() => {
+		quitarAviso();
+		cantidadConvertir = $("#dineroInput").val();
+		cantidadConvertirName = $("#dineroInput").attr("name");
+		if (isNaN(cantidadConvertir) || (cantidadConvertir < 0) || cantidadConvertir == "") {
+			aviso(`${cantidadConvertirName} No válido!!`, "alert");
+		} else {
+			cantidadDestino = JSON.parse(localStorage.getItem("cryptoDestinoCantidad"));
+			cantidadOrigen = JSON.parse(localStorage.getItem("cryptoOrigenCantidad"));
+			cryptoOrigenUSD = JSON.parse(localStorage.getItem("cryptoOrigenUSD"));
+			cryptoDestinoUSD = JSON.parse(localStorage.getItem("cryptoDestinoUSD"));
+			console.log(cryptoOrigenUSD);
+			console.log(cryptoDestinoUSD);
+			resultadoOrigen=cantidadOrigen-cantidadConvertir;
+			let dolaresEnviar=(cantidadConvertir*cryptoOrigenUSD/cantidadOrigen);
+			let dolaresTotal=parseFloat(cryptoDestinoUSD)+parseFloat(dolaresEnviar);
+			resultadoDestino=(dolaresTotal*cantidadDestino/cryptoDestinoUSD);
+			console.log(resultadoOrigen);
+			console.log(resultadoDestino);
+		}
+	});
 
 });
+
+
+
+function aviso(mensaje, tipo) {
+	if (tipo == "alert") {
+		$("#formulario").append(`
+	<div class="avisos alert alert-danger alert-dismissible fade show d-flex align-items-center mt-3" role="alert">
+		  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+			    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+		  </svg>
+		  <div>
+		    ${mensaje}
+		  </div>
+		<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+	</div>
+	`)
+	} else {
+		$("#formulario").append(`
+	<div class="avisos alert alert-primary d-flex align-items-center mt-3" role="alert">
+		  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+			    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+		  </svg>
+		  <div>
+		    ${mensaje}
+		  </div>
+	</div>
+	`)
+	}
+}
+
+function quitarAviso() {
+	$(".avisos").remove();
+}
